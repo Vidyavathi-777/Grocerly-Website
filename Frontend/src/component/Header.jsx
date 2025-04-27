f6a362bb655e62dbe80f74de35f16068d7602768
Vidyavathi-777 <vidyavathidigadari_jtbb2@jtdfoundation.org>
2025-04-27 22:36:03 +0530

    accout

--- Frontend/src/component/Header.jsx
+++ Frontend/src/component/Header.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState , useEffect} from "react";
 import logo from "../assets/grocerly-logo.png";
 import Search from "./Search";
 import { Link, useLocation, useNavigate } from "react-router-dom";
